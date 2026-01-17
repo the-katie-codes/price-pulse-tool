730a7ac2d65e3effef1c3db3dad16f23b4c8aa0f
the-katie-codes <kateryna.bondarenko.work@gmail.com>
2026-01-17 19:05:05 +0100

    Calculate price_change, percent_change, mean_price and save processed CSV

--- main.py
+++ main.py
@@ -20,3 +20,20 @@ data = pd.read_csv('data/pricepulse_sample_data.csv')
 # -------------------------
 data["old_price"] = pd.to_numeric(data["old_price"], errors="coerce")
 data["new_price"] = pd.to_numeric(data["new_price"], errors="coerce")
+
+# -------------------------
+# Step 3: Calculate Metrics
+# -------------------------
+
+# Calculate price changes
+data["price_change"] = round(data["new_price"] - data["old_price"], 2)
+
+# Calculate price change in percentage
+data["percent_change"] = np.where(
+    data["old_price"] != 0,
+    data["price_change"] / data["old_price"] * 100,
+    np.nan
+)
+
+# Calculate mean price
+data["mean_price"] = round((data["old_price"] + data["new_price"]) / 2, 2)
